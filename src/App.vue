219e17646ea85456e19a6f948a2d5ab808a4738c
Mengchenxi <290815385@qq.com>
2022-07-13 12:20:44 +0800

    修改文件

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
+    <!-- 移动端项目适配步骤 -->
+    <!-- 1.rem动态切换 不管处于什么分辨率 1rem 始终是1/10视口大小 -->
+    <!-- 下载amfe-flexible -->
+    <!-- 2.px->rem -->
+    <!-- 下载postcss-pxtorem -->
+    <!-- vant官网 版本5.1.1-->
     <h1>黑马头条</h1>
     <span class="toutiao toutiao-dianzan"></span>
     <van-button type="primary">主要按钮</van-button>
